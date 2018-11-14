4fef4d24b1e9715f443c6f8f26f5212d42a0b406
muhammad.hamza <muhammad.hamza@rolustech.net>
2018-11-14 11:25:54 +0500

    initial commit: created project

---
+++ app/utils/metadata.js
@@ -0,0 +1,3 @@
+export default function metadata() {
+  return true;
+}
---
+++ tests/unit/utils/metadata-test.js
@@ -0,0 +1,11 @@
+import metadata from 'ember-portal/utils/metadata';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | metadata', function(hooks) {
+
+  // Replace this with your real tests.
+  test('it works', function(assert) {
+    let result = metadata();
+    assert.ok(result);
+  });
+});
