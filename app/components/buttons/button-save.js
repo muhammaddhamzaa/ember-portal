6d1d8f2f949ad55f0ab8d98bf9c963e1c4a5a57f
muhammad.hamza <muhammad.hamza@rolustech.net>
2018-11-15 17:56:37 +0500

    Fixed issues with rendering components, buttons being rendered

--- app/components/buttons/button-save.js
+++ app/components/buttons/button-save.js
@@ -1,4 +1,10 @@
 import Component from '@ember/component';
+import MD from '../../utils/metadata';
 
 export default Component.extend({
+  init: function(){
+    this._super(...arguments);
+    this.metaData = MD.create().getMetaViewStuff("Leads", "buttons");
+    console.log(this.metaData);
+  }
 });
---
+++ app/components/fields-list.js
@@ -0,0 +1,5 @@
+import Component from '@ember/component';
+import MD from "../utils/metadata";
+
+export default Component.extend({
+});
